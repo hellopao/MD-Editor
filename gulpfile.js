303ad4b821c5e0d58f68a61e24f2c73f13af5bca
hellopao <qinjia@outlook.com>
2014-10-15 19:26:35 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task('pack',function() {
-    gulp.src(['./js/*.js','!./js/showdown.js'])
-        .pipe(concat('wmd.js'))
-        .pipe(gulp.dest('./'))
+    gulp.src(['./js/showdown.js','./js/wmd.js','./js/translator.js','./js/*.js'])
+        .pipe(concat('editor.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./js'))
 })
